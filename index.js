6d317611882d17b66c92a93fc6599db8744798a2
chensunn <424448083@qq.com>
2016-11-23 17:20:36 +0800

    draw x & o

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ function DrawCanvas (parent) {
   this.canvas.height = 600;
   parent.appendChild(this.canvas);
   this.ctx = this.canvas.getContext('2d');
-  
+  this.isX = false;
+
   this.drawBackground();
 };
 
@@ -28,16 +29,66 @@ DrawCanvas.prototype.drawRect = function (x, y) {
 
 //判断鼠标落入的矩形框坐标
 DrawCanvas.prototype.mouseLocation = function (x, y) {
-  
-}
+  if (x >= 150 && x <250) {
+    if (y >= 150 && y < 250) {
+      return {x: 150, y: 150};
+    } else if (y >= 250 && y < 350) {
+      return {x: 150, y: 250};
+    } else if (y >= 350 && y <= 450) {
+      return {x: 150, y: 350};
+    }
+  } else if (x >= 250 && x < 350) {
+    if (y >= 150 && y < 250) {
+      return {x: 250, y: 150};
+    } else if (y >= 250 && y < 350) {
+      return {x: 250, y: 250};
+    } else if (y >= 350 && y <= 450) {
+      return {x: 250, y: 350};
+    }
+  } else if (x >= 350 && x <= 450) {
+    if (y >= 150 && y < 250) {
+      return {x: 350, y: 150};
+    } else if (y >= 250 && y < 350) {
+      return {x: 350, y: 250};
+    } else if (y >= 350 && y <= 450) {
+      return {x: 350, y: 350};
+    }  
+  }
+};
 
 //判断鼠标点击位置，在当前矩形中绘图
-DrawCanvas.prototype.drawX = function (x, y) {
+DrawCanvas.prototype.drawX = function (loc) {
+  this.ctx.beginPath();
+  this.ctx.moveTo(loc.x, loc.y);
+  this.ctx.lineTo(loc.x + 100, loc.y + 100);
+  this.ctx.moveTo(loc.x + 100, loc.y);
+  this.ctx.lineTo(loc.x, loc.y + 100);
+  this.ctx.stroke();
+};
 
-}
+DrawCanvas.prototype.drawO = function (loc) {
+  this.ctx.beginPath();
+  var circle = {
+    x: loc.x + 50,
+    y: loc.y + 50,
+    r: 25
+  };
+  this.ctx.arc(circle.x, circle.y, circle.r, 0, Math.PI * 2, true);
+  this.ctx.stroke();
+};
 
-DrawCanvas.prototype.drawO = function (x, y) {
+var myCanvas = new DrawCanvas(document.body);
 
-}
-
-var canvas = new DrawCanvas(document.body);
+document.addEventListener("click", function (event) {
+  var bbox = myCanvas.canvas.getBoundingClientRect();
+  var x = (event.clientX - bbox.left) * (myCanvas.canvas.width / bbox.width);
+  var y = (event.clientY - bbox.top) * (myCanvas.canvas.width / bbox.width);
+  var locObj = myCanvas.mouseLocation(x, y);
+  if (myCanvas.isX) {
+    myCanvas.drawX(locObj);
+    myCanvas.isX = false;
+  } else {
+    myCanvas.drawO(locObj);
+    myCanvas.isX = true;
+  }
+});
